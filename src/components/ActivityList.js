3afe7195a66ae83c9a154e2b328e47436863888d
barry <barryrimell@gmail.com>
2024-10-26 22:38:14 +0100

    better logging

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -8,7 +8,9 @@ const ActivityList = () => {
         try {
             const response = await axios.get('https://7w5y9pjq74.execute-api.us-east-1.amazonaws.com/Prod/Activities?UserId=user123');
             console.log(response.data); // Log the API response
-            setActivities(response.data.activities.activities || []); // Adjust this based on actual response
+            console.log(response.data.activities)
+            console.log(response.data.activities.activities)
+            setActivities(response.data.activities || []); // Adjust this based on actual response
         } catch (error) {
             console.error('Error fetching activities:', error);
         }
@@ -20,8 +22,7 @@ const ActivityList = () => {
 
     return (
         <div>
-            <h1>Activities</h1>
-            <h3>{{activities}}</h3>
+            <h1>Activities</h1>            
             {Array.isArray(activities) && activities.length > 0 ? (
                 activities.map(activity => (
                     <div key={activity.ActivityId}>{activity.ActivityName}</div>
