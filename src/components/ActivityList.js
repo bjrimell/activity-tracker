3cea8ca6e4579d3cb1bff2004dfd5c721cbaa3cb
barry <barryrimell@gmail.com>
2024-10-27 12:14:42 +0000

    fix api path and freq deprecation logic

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -14,14 +14,14 @@ const ActivityList = () => {
         }
     };
 
-    const reduceFrequency = async (activityId) => {
+    const reduceFrequency = async (activityId, frequency) => {
         try {
             // Send a PUT request to update the frequency
-            const response = await axios.put(`https://7w5y9pjq74.execute-api.us-east-1.amazonaws.com/Prod/activity/${activityId}`, {
+            const response = await axios.put(`https://7w5y9pjq74.execute-api.us-east-1.amazonaws.com/Prod/Activity/${activityId}`, {
                 UserId: 'user123',
                 ActivityId: activityId,
                 // You may want to fetch the current frequency from the activities state, decrement it, and send it
-                Frequency: 1 // This can be replaced by current frequency - 1
+                Frequency: frequency-1 // This can be replaced by current frequency - 1
             });
             console.log(response.data);
             // Fetch the updated activities after reducing frequency
@@ -43,7 +43,7 @@ const ActivityList = () => {
                     <div key={activity.ActivityId}>
                         <h3>{activity.ActivityName}</h3>
                         <p>Frequency: {activity.Frequency}</p>
-                        <button onClick={() => reduceFrequency(activity.ActivityId)}>Reduce Frequency</button>
+                        <button onClick={() => reduceFrequency(activity.ActivityId, activity.Frequency)}>Reduce Frequency</button>
                     </div>
                 ))
             ) : (
