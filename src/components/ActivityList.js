bc18ff6e36f80bb464321284a9b6df75b794c43a
barry <barryrimell@gmail.com>
2024-10-27 13:42:38 +0000

    style activity list component

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import styles from './ActivityList.module.css'; // Import the CSS module
 
 const ActivityList = () => {
     const [activities, setActivities] = useState([]);
@@ -14,7 +15,7 @@ const ActivityList = () => {
         }
     };
 
-    const incrementCompletedCount = async (activityId, frequency) => {
+    const incrementCompletedCount = async (activityId) => {
         try {
             // Send a PUT request to update the completed count
             const response = await axios.put(`https://7w5y9pjq74.execute-api.us-east-1.amazonaws.com/Prod/Activity/${activityId}`, {
@@ -34,26 +35,28 @@ const ActivityList = () => {
     }, []);
 
     return (
-        <div>
+        <div className={styles.activityList}>
             <h1>Activities</h1>
             {Array.isArray(activities) && activities.length > 0 ? (
                 activities.map(activity => (
-                    <div key={activity.ActivityId}>
+                    <div key={activity.ActivityId} className={styles.activity}>
                         <h3>{activity.ActivityName}</h3>
                         <p>Today's Goal: {activity.Frequency}</p>
                         <p>Today's Completed Count: {activity.CompletedCount}</p>
 
                         {activity.CompletedCount === 0 && (
-                            <p style={{ color: 'orange' }}>Get started today!</p>
+                            <p className={`${styles.message} ${styles.nagging}`}>Get started today!</p>
                         )}
                         {activity.CompletedCount === activity.Frequency && (
-                            <p style={{ color: 'green' }}>Great job! You've met your goal for today!</p>
+                            <p className={`${styles.message} ${styles.success}`}>Great job! You've met your goal for today!</p>
                         )}
                         {activity.CompletedCount > activity.Frequency && (
-                            <p style={{ color: 'blue' }}>Amazing! You've exceeded your goal!</p>
+                            <p className={`${styles.message} ${styles.exceeded}`}>Amazing! You've exceeded your goal!</p>
                         )}
 
-                        <button onClick={() => incrementCompletedCount(activity.ActivityId, activity.Frequency)}>I've just done this activity</button>
+                        <button className={styles.button} onClick={() => incrementCompletedCount(activity.ActivityId)}>
+                            I've just done this activity
+                        </button>
                     </div>
                 ))
             ) : (
